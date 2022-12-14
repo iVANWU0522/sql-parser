6ae5b8e3c8866a3be7439cd24bf807aa1072db7f
Ivan Wu <ivan.wu@elmosoftware.com.au>
2022-12-15 10:05:19 +1100

    fix the issue

--- index.js
+++ index.js
@@ -14,8 +14,13 @@ exports.handler = (event, context, callback) => {
     const parsed = JSON.parse(zlib.gunzipSync(payload).toString('utf8'));
     console.log('Decoded payload:', JSON.stringify(parsed));
 
-    const eventMessage = parsed.logEvents[0].message;
+    parsed.logEvents.forEach((logEvent) => {
+        processMessage(logEvent.message, callback);
+    });
+    return `Successfully processed ${parsed.logEvents.length} log events.`;
+};
 
+const processMessage = (message, callback) => {
     const timeRegex = /\# Time: (\d*-\d*-\d*T\d*:\d*:\d*.\d*Z)\#?/g;
     const userRegex = /\# User@Host: (\S*\[\S*\] @  \[\d*.\d*.\d*.\d*])/g;
     const idRegex = /Id:\s\d*/g;
@@ -23,35 +28,35 @@ exports.handler = (event, context, callback) => {
     const lockTimeRegex = /Lock_time: (\d|\.)*\s+Rows/g
     const rowsSentRegex = /Rows_sent: (\d|\.)*\s+Rows/g;
     const rowsExaminedRegex = /Rows_examined: (\d|\.)*\s+/g;
-    const queryRegex = /use\s+(.|(\r\n|\r|\n))*/g;
+    const queryRegex = /(use|USE|set|SET)\s+(.|(\r\n|\r|\n))*/g;
 
-    const time = eventMessage.match(timeRegex)[0].split(" ")[2];
-    const user = eventMessage.match(userRegex)[0].split(" ")[2];
-    const id = eventMessage.match(idRegex)[0].split(" ")[1];
-    const queryTime = eventMessage.match(queryTimeRegex)[0].split(" ")[2];
-    const lockTime = eventMessage.match(lockTimeRegex)[0].split(" ")[1];
-    const rowsSent = eventMessage.match(rowsSentRegex)[0].split(" ")[1];
-    const rowsExamined = eventMessage.match(rowsExaminedRegex)[0].split(" ")[1];
-    const query = eventMessage.match(queryRegex)[0];
+    const time = message.match(timeRegex)[0].split(" ")[2];
+    const user = message.match(userRegex)[0].split(" ")[2];
+    const id = message.match(idRegex)[0].split(" ")[1];
+    const queryTime = message.match(queryTimeRegex)[0].split(" ")[2];
+    const lockTime = message.match(lockTimeRegex)[0].split(" ")[1];
+    const rowsSent = message.match(rowsSentRegex)[0].split(" ")[1];
+    const rowsExamined = message.match(rowsExaminedRegex)[0].split(" ")[1];
+    const query = message.match(queryRegex)[0];
 
     const databaseNameRegex = /use\s+(\w*)\;?/g;
 
     const databaseName = query.match(databaseNameRegex)[0].split(" ")[1];
 
-    let str = query;
+    let markedQuery = query;
 
     const regex = /(=|<>|<|>|<=|>=|BETWEEN|between|LIKE|like|IN|in)\s+(\d|'([^']*)'|\('[^']*'\))/g;
 
-    const matchedSlots = str.match(regex);
+    const matchedSlots = markedQuery.match(regex);
 
     if (!matchedSlots) {
         matchedSlots.forEach((slot) => {
             const key = slot.split(" ")[0];
-            str = str.replace(slot, `${key} ****`);
+            markedQuery = markedQuery.replace(slot, `${key} ****`);
         });
     }
 
-    const sql = `INSERT INTO slow_query(time, user, event_log_id, query_time, lock_time, rows_sent, rows_examined, marked_query, original_query, database_name) VALUES('${time}', '${user}', ${id}, ${queryTime}, ${lockTime}, ${rowsSent}, ${rowsExamined}, "${str}", "${query}", '${databaseName}');`;
+    const sql = `INSERT INTO slow_query(time, user, event_log_id, query_time, lock_time, rows_sent, rows_examined, marked_query, original_query, database_name) VALUES('${time}', '${user}', ${id}, ${queryTime}, ${lockTime}, ${rowsSent}, ${rowsExamined}, "${markedQuery}", "${query}", '${databaseName}');`;
     // allows for using callbacks as finish/error-handlers
     // context.callbackWaitsForEmptyEventLoop = false;
 
@@ -65,6 +70,4 @@ exports.handler = (event, context, callback) => {
             con.end(function (err) { callback(err, results); });
         }
     });
-
-    return `Successfully processed ${parsed.logEvents.length} log events.`;
-};
+}
