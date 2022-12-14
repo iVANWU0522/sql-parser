e9962b81c871f7f592377c4a558984d12568f30d
Ivan Wu <ivan.wu@elmosoftware.com.au>
2022-12-15 10:19:56 +1100

    fix

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const processMessage = (message, callback) => {
 
     const databaseNameRegex = /use\s+(\w*)\;?/g;
 
-    const databaseName = query.match(databaseNameRegex) ? query.match(databaseNameRegex)[0].split(" ")[1] : "unknown";
+    const databaseName = query.match(databaseNameRegex) ? query.match(databaseNameRegex)[0].split(" ")[1] : null;
 
     let markedQuery = query;
 
